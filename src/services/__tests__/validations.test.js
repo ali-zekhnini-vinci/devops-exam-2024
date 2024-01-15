793b31d366dff68cb9080866eee82bba1d999ae9
Zekhnini <ali.zekhnini@student.vinci.be>
2024-01-15 17:33:00 +0100

    Add the first test

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,9 +1,18 @@
-// TODO: Create tests suite for validation function
 const { isValid } = require('../validations');
 
 describe('validations tests suites - isValid', () => {
     test('should return true or false as the function is defined', () => {
-        const result = isValid();
+        const result = isValid('somegamertag');
         expect(result).toBeDefined();
+    });
+
+    test('should return false for gamertags with length less than 8 characters', () => {
+        const result = isValid('azes');
+        expect(result).toBeFalsy();
+    });
+
+    test('should return true for gamertags with length equal to or greater than 8 characters', () => {
+        const result = isValid('azqsdwcgertyu');
+        expect(result).toBeTruthy();
     });
 });
